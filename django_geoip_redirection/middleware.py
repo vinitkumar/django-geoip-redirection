906b2ae893d8fd55706ac5becc9fe5f4073168bf
vinit Kumar <vinit.kumar@changer.nl>
2014-04-26 19:00:22 +0530

    bump version, cleanup code and add bit documentation

--- django_geoip_redirection/middleware.py
+++ django_geoip_redirection/middleware.py
@@ -1,26 +1,36 @@
 # -*- coding: utf-8 -*-
 import pygeoip
-from django.utils.functional import SimpleLazyObject
 from django.conf import settings
 from django.http import HttpResponseRedirect
 
-def get_country_request(ip):
 
+def get_country_request(ip_address):
+    """
+    Looks up the database to find out country of the incoming IP.
+    """
     file_path = settings.PROJECT_ROOT + '/data/GeoIP.dat.dat'
-    gi = pygeoip.GeoIP(file_path)
-    country = gi.country_name_by_addr(ip)
+    geo_object = pygeoip.GeoIP(file_path)
+    country = geo_object.country_name_by_addr(ip_address)
     return country
 
+
 class LocationMiddleWare(object):
+    """
+    Middleware to redirect user as per country returned by get_country_request
+    """
 
     def process_request(self, request):
-    # NOTICE: This will make sure redirect loop is broken.
+        """
+        Process incoming requests
+        """
+        #NOTICE: This will make sure redirect loop is broken.
+        # The list is same as the languages in your django settings
         if request.path[:4] in ["/en/", "/nl/", "/in/"]:
             return None
         if 'HTTP_X_FORWARDED_FOR' in request.META:
             request.META['REMOTE_ADDR'] = request.META['HTTP_X_FORWARDED_FOR']
-        ip = request.META['REMOTE_ADDR']
-        country = get_country_request(ip)
+        ip_address = request.META['REMOTE_ADDR']
+        country = get_country_request(ip_address)
         if country == "India":
             return HttpResponseRedirect('/in/')
         elif country == "Netherlands":
@@ -28,4 +38,3 @@ class LocationMiddleWare(object):
         else:
             return HttpResponseRedirect('/en/')
         return None
-
